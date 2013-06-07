62af0fdcfa251155428d96aaa8c424e16ad4ad00
Eric Cobb <ericdcobb@gmail.com>
2013-06-07 12:19:32 -0600

    printing files, file size and extension limits

--- fix_files.rb
+++ fix_files.rb
@@ -1,9 +1,20 @@
 clean_directory =  "#{ARGV[0]}/**/*".gsub("\\","/" )
+extension = ""
+
+if ARGV[1].start_with? "." 
+	extension = ARGV[1] 
+else 
+	extension = ".#{ARGV[1]}"
+end
+
+min_file_size = ARGV[2].to_i
 
 entries = Dir.glob(clean_directory)
 
 entries.each do |item|
-	next if item.eql? '.' or item.eql? '..' or File.directory? item
+	next if item.eql? '.' or item.eql? '..'
+	next if File.directory? item or File.extname(item).casecmp(extension) != 0
+	next if (File.size? item) < min_file_size
 	puts "File Name: #{item} " 
-	puts "File Size #{ File.size? item} "
+	puts "File Size: #{ File.size? item} "
 end
