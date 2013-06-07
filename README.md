0628173af4af3cf429b663c80a30fbfdcd81b734
Eric Cobb <ericdcobb@gmail.com>
2013-06-07 13:02:48 -0600

    renaming files, addign time limit

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ A stupid simple utility for cleaning out a directory.
 
 Example usage:
 
-	ruby cleanup.rb /Users/ericcobb/Desktop/test mov 1073741824
+	ruby cleanup.rb /Users/ericcobb/Desktop/test mov 1073741824 5
 
-Will remove every file in the directory `/Users/ericcobb/Desktop/test` with the extension `mov` and larger that 1 gig.
+Will remove every file in the directory `/Users/ericcobb/Desktop/test` with the extension `mov` and larger that 1 gig, and older than 5 days.
---
+++ cleanup.rb
@@ -0,0 +1,37 @@
+require 'date'
+system("clear")
+
+clean_directory =  "#{ARGV[0]}/**/*".gsub("\\","/" )
+extension = ""
+
+if ARGV[1].start_with? "." 
+	extension = ARGV[1] 
+else 
+	extension = ".#{ARGV[1]}"
+end
+
+min_file_size = ARGV[2].to_i
+
+max_age = ARGV[3].to_i
+delete_date = DateTime.now - max_age
+
+file_count = 0
+total_bytes_to_clean = 0
+
+entries = Dir.glob(clean_directory)
+entries.each do |item|
+	next if item.eql? '.' or item.eql? '..'
+	next if File.directory? item or File.extname(item).casecmp(extension) != 0
+	next if (File.size? item) < min_file_size
+	next if ( DateTime.parse( (File.mtime item).to_s ) )> delete_date
+
+	file_count = file_count + 1
+	total_bytes_to_clean = total_bytes_to_clean + ( File.size? item )
+
+	puts "File Name: #{item} " 
+	puts "File Size: #{ File.size? item} "
+end
+
+total_gigs =  9.31323e-10 * total_bytes_to_clean
+
+puts "Cleaned #{file_count} files to save #{total_gigs} gigs."
