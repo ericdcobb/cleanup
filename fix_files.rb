363591d0b08024c2e27a4718bbc3bda748c70038
Eric Cobb <ericdcobb@gmail.com>
2013-06-07 12:08:43 -0600

    works with windows

--- fix_files.rb
+++ fix_files.rb
@@ -4,5 +4,6 @@ entries = Dir.glob(clean_directory)
 
 entries.each do |item|
 	next if item.eql? '.' or item.eql? '..' or File.directory? item
-	puts "#{item} | #{ File.size? item}" 
+	puts "File Name: #{item} " 
+	puts "File Size #{ File.size? item} "
 end
